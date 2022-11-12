9e32f44041f03f9d7ed052419d82df84ac57ca68
Goki Kanno <goki.kanno@gmail/com>
2022-11-12 23:25:40 +0900

    モーダル作成

---
+++ web-app/script.js
@@ -0,0 +1,66 @@
+'use strict'
+
+
+function clickBtn() {
+  const btn = document.getElementById("form");
+  if (form.style.display == "block") {
+    form.style.display = "none";
+
+
+  } else {
+    form.style.display = "block"
+  }
+}
+
+// ×ボタン
+const modalClose =document.getElementsByClassName('modal_close');
+console.log(modalClose);
+modalClose[0].addEventListener("click", () => {
+  form[0].classList.remove("modal_active");
+})
+
+
+
+console.clear();
+
+{
+
+  const year = 2020;
+  const month = 4; //5月
+
+  function getCalendarHead() {
+    const dates =[];
+    const d=new Date (year ,month,0).getDate();
+    const n=new Date(year, month, 1).getDay();
+    for (let i = 0; i < n; i++) {
+      //30
+      //29,30
+      //28,29,30
+      dates.unshift({
+        date:d - i,
+        isToday:false,
+        isDisabled: true,
+      });
+      
+    }
+    console.log(dates);
+  }
+
+  function getCalendarBody() {
+    const dates = [];//date:日付,day: 曜日
+    const lastDate = new Date(year, month + 1, 0).getDate();
+    for (let i = 1; i <= lastDate; i++) {
+      dates.push({
+        dates: i,
+        isToday: false,
+        isDisabled: false,
+      }
+
+      )
+    }
+    console.log(dates);
+  }
+  // getCalendarBody();
+  getCalendarHead();
+
+}
